9afa765c11ba0c8558436753f4b0f25b8e27c120
Mario Pereira <nikhilpereira25@gmail.com>
2024-05-03 17:10:05 -0400

    update logger function to not log for healthchecks

--- app/common/helpers/logger.ts
+++ app/common/helpers/logger.ts
@@ -2,24 +2,27 @@ import { Request, Response, NextFunction } from "express";
 
 // Handle GET requests to /api route
 const logger = (req: Request, res: Response, next: NextFunction): void => {
-	// log time taken to process request
-	const start = Date.now();
-	res.on("finish", () => {
-		const elapsed = Date.now() - start;
-		console.log(
-			`${req.method}:${res.statusCode}: '${req.protocol}://${req.get("host")}${
-				req.originalUrl
-			}' at: '${new Date().toLocaleString("en-US", {
-				timeZone: "America/New_York",
-			})}' from ${
-				req.ip ||
-				req.headers["x-forwarded-for"] ||
-				req.socket.remoteAddress ||
-				req.ip ||
-				null
-			} | ${elapsed}ms | ${res.get("Content-Length") || 0} b`
-		);
-	});
+	// if not /ping
+	if (req.url !== "/ping") {
+		// log time taken to process request
+		const start = Date.now();
+		res.on("finish", () => {
+			const elapsed = Date.now() - start;
+			console.log(
+				`${req.method}:${res.statusCode}: '${req.protocol}://${req.get("host")}${
+					req.originalUrl
+				}' at: '${new Date().toLocaleString("en-US", {
+					timeZone: "America/New_York",
+				})}' from ${
+					req.ip ||
+					req.headers["x-forwarded-for"] ||
+					req.socket.remoteAddress ||
+					req.ip ||
+					null
+				} | ${elapsed}ms | ${res.get("Content-Length") || 0} b`
+			);
+		});
+	}
 	next();
 };
 
