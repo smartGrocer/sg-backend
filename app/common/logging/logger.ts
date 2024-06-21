0bb27822046bcec6f509fe3633736a0d753c3084
Mario Pereira <nikhilpereira25@gmail.com>
2024-06-21 18:55:19 -0400

    Change winston logger to be prettier

--- app/common/logging/logger.ts
+++ app/common/logging/logger.ts
@@ -1,48 +1,69 @@
 import winston from "winston";
 import getSecret from "../helpers/getSecret";
 
+const logFormat = winston.format.printf(
+	({ timestamp, level, message, service, error }) => {
+		return `${timestamp} [${service}] ${level}: ${message} ${
+			error ? `: ${error.stack}` : ""
+		}`;
+	}
+);
 const logger = winston.createLogger({
 	level: "debug",
+
 	format: winston.format.combine(
-		winston.format.errors({ stack: true }),
-		winston.format.simple()
+		winston.format.timestamp(),
+		winston.format.errors({ stack: false }),
+		logFormat
 	),
 	defaultMeta: { env: getSecret("NODE_ENV") },
 	transports: [
-		new winston.transports.Console(),
+		new winston.transports.Console({
+			format: winston.format.colorize({ all: true }),
+		}),
 		new winston.transports.File({
 			filename: "logs/error.log",
 			level: "error",
 			format: winston.format.combine(
 				winston.format.timestamp(),
-				winston.format.json()
+				winston.format.errors({ stack: true }),
+				logFormat
 			),
 		}),
 		new winston.transports.File({
 			filename: "logs/combined.log",
+			level: "debug",
 			format: winston.format.combine(
 				winston.format.timestamp(),
-				winston.format.json()
+				winston.format.errors({ stack: true }),
+
+				logFormat
 			),
 		}),
 	],
 	exceptionHandlers: [
-		new winston.transports.Console(),
+		new winston.transports.Console({
+			format: winston.format.colorize({ all: true }),
+		}),
 		new winston.transports.File({
 			filename: "logs/exceptions.log",
 			format: winston.format.combine(
 				winston.format.timestamp(),
-				winston.format.json()
+				winston.format.errors({ stack: true }),
+				logFormat
 			),
 		}),
 	],
 	rejectionHandlers: [
-		new winston.transports.Console(),
+		new winston.transports.Console({
+			format: winston.format.colorize({ all: true }),
+		}),
 		new winston.transports.File({
 			filename: "logs/rejections.log",
 			format: winston.format.combine(
 				winston.format.timestamp(),
-				winston.format.json()
+				winston.format.errors({ stack: true }),
+				logFormat
 			),
 		}),
 	],
