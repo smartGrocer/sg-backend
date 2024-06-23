c5dc8e9bddd47b88a1bf4ad59add0d427b41c569
Jorge Machuca <jmachuca06@gmail.com>
2024-06-23 09:57:01 -0400

    Frontend setup
    [X] vite
    [X] tailwind
    [X] daisyUI

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<button className="btn">Daisy Works</button>
+		</>
+	);
+}
+
+export default App;
