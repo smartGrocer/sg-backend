5e2d2c30af35b5259dad2de71909f79f41bad311
Mario Pereira <nikhilpereira25@gmail.com>
2024-07-01 18:01:22 -0400

    remove cleanProductWithPrices in favor of in query cleanup

--- app/crawler/controllers/getSpecificProduct.ts
+++ app/crawler/controllers/getSpecificProduct.ts
@@ -1,51 +1,6 @@
 import { Request, Response } from "express";
-
 import logger from "../../common/logging/logger";
 import Product from "../../common/db/schema/product";
-import {
-	IPriceHistory,
-	IProductDataWithPriceHistory,
-} from "../../common/types/common/product";
-import cleanMongoDoc from "../../common/helpers/cleanMongoDoc";
-import morphPriceHistoryArray from "../../common/helpers/morphPriceHistoryArray";
-
-const cleanProductWithPrices = (
-	productWithPrice: IProductDataWithPriceHistory[]
-) => {
-	const { priceHistory, ...product } =
-		productWithPrice[0] as IProductDataWithPriceHistory;
-
-	const cleanedPrices = priceHistory.map((doc) => {
-		const cleanedPriceDoc = cleanMongoDoc({
-			doc,
-			keysToRemove: ["_id", "__v"],
-		}) as IPriceHistory;
-
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		const { history, product_num, ...rest } = cleanedPriceDoc;
-
-		const formattedPriceArray = morphPriceHistoryArray(history);
-
-		const formattedPriceHistory = {
-			...rest,
-			history: formattedPriceArray,
-		};
-
-		return formattedPriceHistory;
-	});
-
-	const cleanedProduct = cleanMongoDoc({
-		doc: product,
-		keysToRemove: ["_id", "__v"],
-	});
-
-	const productWithCleanedPrices = {
-		...cleanedProduct,
-		priceHistory: cleanedPrices,
-	};
-
-	return productWithCleanedPrices;
-};
 
 const getProductWithPrices = async (productId: string) => {
 	const productWithPrices = await Product.aggregate([
@@ -56,7 +11,7 @@ const getProductWithPrices = async (productId: string) => {
 		},
 		{
 			$lookup: {
-				from: "prices", // The collection name in MongoDB
+				from: "prices",
 				localField: "product_num",
 				foreignField: "product_num",
 				as: "priceHistory",
@@ -70,7 +25,7 @@ const getProductWithPrices = async (productId: string) => {
 		},
 		{
 			$lookup: {
-				from: "stores", // The collection name in MongoDB
+				from: "stores",
 				localField: "priceHistory.store_num",
 				foreignField: "store_num",
 				as: "priceHistory.store",
@@ -96,10 +51,30 @@ const getProductWithPrices = async (productId: string) => {
 				updatedAt: { $first: "$updatedAt" },
 				priceHistory: {
 					$push: {
-						_id: "$priceHistory._id",
 						product_num: "$priceHistory.product_num",
 						store_num: "$priceHistory.store_num",
-						history: "$priceHistory.history",
+						history: {
+							$map: {
+								input: "$priceHistory.history",
+								as: "hist",
+								in: {
+									$arrayToObject: [
+										[
+											{
+												k: {
+													$dateToString: {
+														format: "%Y-%m-%dT%H:%M:%S.%LZ",
+														date: "$$hist.date",
+														timezone: "UTC",
+													},
+												},
+												v: "$$hist.amount",
+											},
+										],
+									],
+								},
+							},
+						},
 						store: {
 							chain_name: "$priceHistory.store.chain_name",
 							store_num: "$priceHistory.store.store_num",
@@ -108,6 +83,39 @@ const getProductWithPrices = async (productId: string) => {
 							latitude: "$priceHistory.store.latitude",
 							longitude: "$priceHistory.store.longitude",
 							store_name: "$priceHistory.store.store_name",
+						},
+					},
+				},
+			},
+		},
+		{
+			$project: {
+				_id: 0,
+				product_num: 1,
+				chain_brand: 1,
+				product_brand: 1,
+				product_name: 1,
+				product_link: 1,
+				product_image: 1,
+				description: 1,
+				createdAt: 1,
+				updatedAt: 1,
+				priceHistory: {
+					$map: {
+						input: "$priceHistory",
+						as: "price",
+						in: {
+							store_num: "$$price.store_num",
+							store: {
+								chain_name: "$$price.store.chain_name",
+								store_num: "$$price.store.store_num",
+								formatted_address:
+									"$$price.store.formatted_address",
+								latitude: "$$price.store.latitude",
+								longitude: "$$price.store.longitude",
+								store_name: "$$price.store.store_name",
+							},
+							history: "$$price.history",
 						},
 					},
 				},
@@ -136,11 +144,8 @@ const getSpecificProduct = async (req: Request, res: Response) => {
 			});
 		}
 
-		const cleanedProductWithPrices =
-			cleanProductWithPrices(productWithPrice);
-
 		return res.status(200).json({
-			product: cleanedProductWithPrices,
+			product: productWithPrice[0],
 		});
 	} catch (error) {
 		logger.error(error);
