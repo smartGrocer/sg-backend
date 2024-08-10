2508f1e70f803409e82b725910054b6b1763b4a2
Mario Pereira <nikhilpereira25@gmail.com>
2024-08-10 02:31:04 -0400

    Update scrapeStore.ts

--- backend/app/crawler/controllers/scrapeStore.ts
+++ backend/app/crawler/controllers/scrapeStore.ts
@@ -12,7 +12,10 @@ const scrapeStores = async (req: Request, res: Response) => {
 	if (!flagName) {
 		return res.status(400).json({
 			message: `Flag name is required, please provide a flag name as a query parameter like so: /scrape?flag=flag_name`,
-			availableOptions: [...Object.values(LoblawsFlagName)],
+			availableOptions: [
+				...Object.values(LoblawsFlagName),
+				...Object.values(MetroFlags),
+			],
 		});
 	}
 
